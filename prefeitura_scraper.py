30c3e9c2db9d6ff88824ac33d6fb6441bdfea330
Gustavo Lopes <guhrlopes999@gmail.com>
2026-05-13 08:33:56 -0300

    Removidas importações desnecessárias

--- prefeitura_scraper.py
+++ prefeitura_scraper.py
@@ -1,12 +1,10 @@
 import datetime
 
-from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 class PrefeituraScraper:
     def __init__(self, url, driver, data_atual):
