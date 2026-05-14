4e2cd08f202fa1f2be90213764dea35b2de39efd
Gustavo Lopes <guhrlopes999@gmail.com>
2026-05-14 17:05:31 -0300

    Fixed: corrigido bug ao baixar arquivo em segundo plano

--- browser.py
+++ browser.py
@@ -23,4 +23,19 @@ class Browser:
     def criar_driver_segundo_plano():
         browser_options = Options()
         browser_options.add_argument("--headless=new")
-        return webdriver.Chrome(options=browser_options)
+        browser_options.add_argument("--window-size=1920,1080")
+        browser_options.add_argument("--disable-gpu")
+        browser_options.add_argument("--no-sandbox")
+        browser_options.page_load_strategy = 'eager'
+        prefs = {
+            "download.default_directory": "C:\\Users\\gusta\\Downloads\\pref-scraper\\files",
+            "download.prompt_for_download": False,
+            "plugins.always_open_pdf_externally": True
+        }
+        browser_options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(options=browser_options)
+        driver.execute_cdp_cmd("Browser.setDownloadBehavior", {
+            "behavior": "allow",
+            "downloadPath": "C:\\Users\\gusta\\Downloads\\pref-scraper\\files"
+        })
+        return driver
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import time
 from browser import Browser
 from pages.home_page import HomePage
 
-driver = Browser.criar_driver_primeiro_plano()
+driver = Browser.criar_driver_segundo_plano()
 
 inscricao_com_debito_vencido = "0001030890194001"
 inscricao_sem_debito_vencido = "0001030830062001"
 inscricao_sem_debito = "0001030830032001"
 inscricao_com_debito_vencido_muitas_paginas = "0001030920287001"
 
-caminho_salvar_arquivo = "C:\\Users\\gusta\\Downloads\\pref-scraper\\files\\"
+caminho_salvar_arquivo = "C:\\Users\\gusta\\Downloads\\pref-scraper\\files"
 
 home_page = HomePage(driver)
 home_page.acessar_pagina_inicial_imovel()
