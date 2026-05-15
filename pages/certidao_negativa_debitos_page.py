93dbb22a076387467ec98b8798bc315146846866
Gustavo Lopes <guhrlopes999@gmail.com>
2026-05-15 15:41:45 -0300

    Criada certidao_negativa_debitos_page.py

---
+++ pages/certidao_negativa_debitos_page.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+from config.selectors import ID_BOTAO_CERTIDAO_NEGATIVA_DEBITOS
+from utils.waits import esperar_elemento_visivel, esperar_download_completo
+
+
+class CertidaoNegativaDebitosPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def gerar_pdf_certidao_negativa_debitos(self):
+
+        imovel = self.driver.dados_imovel
+        nome_arquivo = f"{imovel['bairro']}_{imovel['quadra']}_{imovel['lote']} - certidao_negativa.pdf"
+
+        esperar_elemento_visivel(self.driver, By.ID, ID_BOTAO_CERTIDAO_NEGATIVA_DEBITOS).click()
+
+        return esperar_download_completo(self.driver.pasta_download, nome_arquivo=nome_arquivo)
