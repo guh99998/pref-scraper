cabbb383515f4cbfe18bdc3e147f5d9aa20ccb80
Gustavo Lopes <guhrlopes999@gmail.com>
2026-05-15 12:13:18 -0300

    Adicionado itens no selectors.py

--- config/selectors.py
+++ config/selectors.py
@@ -2,7 +2,11 @@ XPATH_CAMPO_INSCRICAO = "/html/body/div[13]/div[2]/form/table/tbody/tr[1]/td[2]/
 
 XPATH_BOTAO_VALIDAR_INSCRICAO = "/html/body/div[13]/div[2]/form/span[4]/div/button[1]"
 
+XPATH_BOTAO_VALIDAR_DADOS = "/html/body/div[16]/div[2]/form/span[4]/div/button[1]"
+
 XPATH_PAGINA_INFORMACOES_COMPLETAS = "/html/body/div[9]/div/div[3]/form/div[2]/div/ul/li[1]/a"
+
+ID_FORMULARIO_PAGINA_INFORMACOES_COMPLETAS = "compInformarImovel:formNumero:itCnpjCpf2"
 
 XPATH_PAGINA_DEBITOS = "/html/body/div[9]/div/div[3]/form/div[2]/div/ul/li[2]/a"
 
@@ -28,6 +32,8 @@ ID_ITEMS_TABELA_DE_DEBITOS = "formDebitos:tabViewDebitos:dataTableDados_data"
 
 ID_BOTAO_IMPRIMIR_TODOS_DEBITOS = "formDebitos:tabViewDebitos:j_idt868"
 
+ID_BOTAO_IMPRIMIR_FICHA_CADASTRAL = "formInformacoesCompletas:j_idt649"
+
 CLASSE_COR_AZUL_SEM_DEBITOS_PAGINA_DEBITOS = "ui-state-default ui-corner-all"
 
 ID_PRIMEIRO_VENCIMENTO_PARCELAS_DEBITOS = "formDebitos:tabViewDebitos:dataTableDados:j_idt812"
@@ -41,3 +47,19 @@ LINK_TEXT_IMOBILIARIO = "Imobiliário"
 LINK_TEXT_DEBITOS = "Débitos"
 
 LINK_TEXT_DADOS_GERAIS = "Dados gerais"
+
+XPATH_NOME_PROPRIETARIO_TELA_DADOS ="/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[4]/td[1]/span"
+
+XPATH_DOC_PROPRIETARIO_TELA_DADOS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[4]/td[2]/span"
+
+XPATH_LOGRADOURO_DADOS_GERAIS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[10]/td[1]/span"
+
+XPATH_NUMERO_DADOS_GERAIS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[10]/td[2]/span"
+
+XPATH_BAIRRO_DADOS_GERAIS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[12]/td[1]/span"
+
+XPATH_QUADRA_DADOS_GERAIS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[16]/td[3]"
+
+XPATH_LOTE_DADOS_GERAIS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[16]/td[4]"
+
+XPATH_COMPLEMENTO_DADOS_GERAIS = "/html/body/div[9]/div/div[3]/form/div[2]/div[1]/div/div[1]/table/tbody/tr[10]/td[3]"
