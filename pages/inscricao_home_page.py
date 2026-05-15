f9e97048d2d3499eff89616d3cfd2fa7c3a85bac
Gustavo Lopes <guhrlopes999@gmail.com>
2026-05-15 12:15:56 -0300

    Adicionado novo caminho para página

--- pages/inscricao_home_page.py
+++ pages/inscricao_home_page.py
@@ -1,14 +1,22 @@
 from selenium.webdriver.common.by import By
-from utils.waits import esperar_elemento_clicavel
+
+from pages.informacoes_completas_page import InformacoesCompletasPage
+from utils.waits import esperar_elemento_clicavel, esperar_elemento_visivel
 from pages.debitos_em_aberto_page import DebitosEmAbertoPage
-from config.selectors import XPATH_PAGINA_DEBITOS
+from config.selectors import XPATH_PAGINA_DEBITOS, XPATH_PAGINA_INFORMACOES_COMPLETAS, \
+    ID_FORMULARIO_PAGINA_INFORMACOES_COMPLETAS, ID_CAMPO_CABECALHO_CPF_CNPJ, XPATH_BOTAO_VALIDAR_DADOS
+
 
 class InscricaoHomePage:
     def __init__(self, driver):
         self.driver = driver
 
     def acessar_informacoes_completas(self):
-        pass
+        doc_proprietario = esperar_elemento_visivel(self.driver, By.ID, ID_CAMPO_CABECALHO_CPF_CNPJ).text
+        esperar_elemento_clicavel(self.driver, By.XPATH, XPATH_PAGINA_INFORMACOES_COMPLETAS).click()
+        esperar_elemento_clicavel(self.driver, By.ID, ID_FORMULARIO_PAGINA_INFORMACOES_COMPLETAS).send_keys(doc_proprietario)
+        esperar_elemento_clicavel(self.driver, By.XPATH, XPATH_BOTAO_VALIDAR_DADOS).click()
+        return InformacoesCompletasPage(self.driver)
 
     def acessar_debitos_em_aberto(self):
         esperar_elemento_clicavel(self.driver, By.XPATH, XPATH_PAGINA_DEBITOS).click()
